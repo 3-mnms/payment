49632549e3bf731469b73b3b324b7cde4f7311df
V4N1LL4 <bubble1549@gmail.com>
2025-08-08 13:10:23 +0900

    MNMS-301 :rocket: feat(ci): payment Docker build & dispatch (test)
    - feature/MNMS-301 브랜치 전용 빌드·푸시 워크플로 추가
    - Docker Hub test 태그 빌드 및 GitOps(payment-test) repository_dispatch 전송

--- src/main/java/com/teckit/payment/repository/PaymentEventRepository.java
+++ src/main/java/com/teckit/payment/repository/PaymentEventRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PaymentEventRepository extends JpaRepository<PaymentEvent,String> {
+public interface PaymentEventRepository extends JpaRepository<PaymentEvent,Long> {
     Optional<PaymentEvent> findByPaymentId(String paymentId);
 
 
