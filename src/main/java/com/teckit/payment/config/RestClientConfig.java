4d946a8524948df5174f528d843e3e9ca0491ae1
BitedRadish <a980831@naver.com>
2025-09-08 15:26:03 +0900

    :chore: fix : RestClient setTimeout 설정

--- src/main/java/com/teckit/payment/config/RestClientConfig.java
+++ src/main/java/com/teckit/payment/config/RestClientConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -18,10 +19,16 @@ public class RestClientConfig {
 
     @Bean
     public RestClient PortOneClient(){
+        HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        httpComponentsClientHttpRequestFactory.setConnectTimeout(5000);
+        httpComponentsClientHttpRequestFactory.setReadTimeout(5000);
+
         log.info("✅ Portone api key {}",apiKey);
+
         return RestClient.builder()
                 .baseUrl("https://api.portone.io")
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "PortOne "+apiKey)
+                .requestFactory(httpComponentsClientHttpRequestFactory)
                 .build();
     }
 }
